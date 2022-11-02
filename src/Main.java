2c3bc232428f00ac07f7bf7133a1e72616c6c109
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-03 02:43:26 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        double boy = scanner.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        double kilo = scanner.nextDouble();
+
+        double indeks = kilo / (boy * boy);
+
+        System.out.print("Vücut Kitle İndeksiniz : " + indeks);
     }
 }
