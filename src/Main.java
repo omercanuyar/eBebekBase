eb50093cb03ffce6057ac5239d8f88d800f1210c
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-03 03:09:48 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double armutFiyat = 2.14;
+        double elmaFiyat = 3.67;
+        double domatesFiyat = 1.11;
+        double muzFiyat = 0.11;
+        double patlicanFiyat = 5;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Armut Kaç Kilo ? :");
+        double armutKilo = scanner.nextDouble();
+
+        System.out.println("Elma Kaç Kilo ? :");
+        double elmaKilo = scanner.nextDouble();
+
+        System.out.println("Domates Kaç Kilo ? :");
+        double domatesKilo = scanner.nextDouble();
+
+        System.out.println("Muz Kaç Kilo ? :");
+        double muzKilo = scanner.nextDouble();
+
+        System.out.println("Patlıcan Kaç Kilo ? :");
+        double patlicanKilo = scanner.nextDouble();
+
+        double toplamTutar = (armutKilo*armutFiyat)+(elmaKilo*elmaFiyat)+(domatesKilo*domatesFiyat)+(muzKilo*muzFiyat)+(patlicanKilo*patlicanFiyat);
+
+        System.out.println("Toplam Tutar : "+toplamTutar+" TL");
+
     }
 }
