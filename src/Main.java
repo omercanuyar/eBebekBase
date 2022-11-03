b6337203cc544c549ad587935c0b0e00786fe01f
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-03 03:41:20 +0300

    last

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double armutFiyat = 2.14;
-        double elmaFiyat = 3.67;
-        double domatesFiyat = 1.11;
-        double muzFiyat = 0.11;
-        double patlicanFiyat = 5;
+        // define prices
+        double  pearPrice = 2.14,
+                applePrice = 3.67,
+                tomatoPrice = 1.11,
+                bananaPrice = 0.11,
+                auberginePrice = 5;
 
+        //taking inputs and assigning them
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Armut Kaç Kilo ? :");
-        double armutKilo = scanner.nextDouble();
+        double pearKilogram = scanner.nextDouble();
 
         System.out.println("Elma Kaç Kilo ? :");
-        double elmaKilo = scanner.nextDouble();
+        double appleKilogram = scanner.nextDouble();
 
         System.out.println("Domates Kaç Kilo ? :");
-        double domatesKilo = scanner.nextDouble();
+        double tomatoKilogram = scanner.nextDouble();
 
         System.out.println("Muz Kaç Kilo ? :");
-        double muzKilo = scanner.nextDouble();
+        double bananaKilogram = scanner.nextDouble();
 
         System.out.println("Patlıcan Kaç Kilo ? :");
-        double patlicanKilo = scanner.nextDouble();
+        double aubergineKilogram = scanner.nextDouble();
 
-        double toplamTutar = (armutKilo*armutFiyat)+(elmaKilo*elmaFiyat)+(domatesKilo*domatesFiyat)+(muzKilo*muzFiyat)+(patlicanKilo*patlicanFiyat);
+        //calculating total price
+        double totalPrice = (pearKilogram*pearPrice)+(appleKilogram*applePrice)+(tomatoKilogram*tomatoPrice)+(bananaKilogram*bananaPrice)+(aubergineKilogram*auberginePrice);
 
-        System.out.println("Toplam Tutar : "+toplamTutar+" TL");
+        //printing total price
+        System.out.println("Toplam Tutar : "+totalPrice+" TL");
 
     }
 }
