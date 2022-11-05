e6a87e6c192a81aec2d5bb631480dbdd66e59bd7
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-05 06:19:49 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //taking inputs
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Your Math grade:");
+        float math = scanner.nextFloat();
+
+        System.out.println("Your Physics grade:");
+        float physics = scanner.nextFloat();
+
+        System.out.println("Your Chemistry grade:");
+        float chemistry = scanner.nextFloat();
+
+        System.out.println("Your Turkish grade:");
+        float turkish = scanner.nextFloat();
+
+        System.out.println("Your History grade:");
+        float history = scanner.nextFloat();
+
+        System.out.println("Your Music grade:");
+        float music = scanner.nextFloat();
+
+        //calculating average and print status without using if statement
+        float average = (math + physics + chemistry + turkish + history + music)/6;
+        boolean result = average >= 60;
+
+        String status = result ? "Geçtiniz":"Kaldınız";
+        System.out.println("Ortalamanız: "+average+" "+status);
+
     }
+
 }
