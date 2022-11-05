10004cc6ebd5f3e57a7788777b64c2fc6976f73a
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-05 03:19:49 +0300

    switch to switch :)

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,38 @@ public class Main {
     public static void main(String[] args) {
 
         //taking input and assigning to variables
-        String outputStarter = "Çin Zodyağı Burcunuz: ";
+        String outputBeginning = "Çin Zodyağı Burcunuz: ";
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Doğum Yılınızı Giriniz :");
         int birthdate = scanner.nextInt();
 
-        //if statements for months and printing
-        if (birthdate%12==0){
-            System.out.println(outputStarter+"Maymun");
-        } else if (birthdate%12==1) {
-            System.out.println(outputStarter+"Horoz");
-        } else if (birthdate%12==2) {
-            System.out.println(outputStarter+"Köpek");
-        } else if (birthdate%12==3) {
-            System.out.println(outputStarter+"Domuz");
-        } else if (birthdate%12==4) {
-            System.out.println(outputStarter+"Fare");
-        } else if (birthdate%12==5) {
-            System.out.println(outputStarter+"Öküz");
-        } else if (birthdate%12==6) {
-            System.out.println(outputStarter+"Kaplan");
-        } else if (birthdate%12==7) {
-            System.out.println(outputStarter+"Tavşan");
-        } else if (birthdate%12==8) {
-            System.out.println(outputStarter+"Ejderha");
-        } else if (birthdate%12==9) {
-            System.out.println(outputStarter+"Yılan");
-        } else if (birthdate%12==10) {
-            System.out.println(outputStarter+"At");
-        }else System.out.println(outputStarter+"Koyun");
+        //switch case for months and printing
+        switch (birthdate%12) {
+            case 0:
+                System.out.println(outputBeginning + "Maymun");
+            case 1:
+                System.out.println(outputBeginning + "Horoz");
+            case 2:
+                System.out.println(outputBeginning + "Köpek");
+            case 3:
+                System.out.println(outputBeginning + "Domuz");
+            case 4:
+                System.out.println(outputBeginning + "Fare");
+            case 5:
+                System.out.println(outputBeginning + "Öküz");
+            case 6:
+                System.out.println(outputBeginning + "Kaplan");
+            case 7:
+                System.out.println(outputBeginning + "Tavşan");
+            case 8:
+                System.out.println(outputBeginning + "Ejderha");
+            case 9:
+                System.out.println(outputBeginning + "Yılan");
+            case 10:
+                System.out.println(outputBeginning + "At");
+            case 11:
+                System.out.println(outputBeginning + "Koyun");
+        }
     }
 }
