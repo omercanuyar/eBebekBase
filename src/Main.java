b5988ffdb0ef0a77af3c92f1acc4f10b52493a39
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-05 03:36:25 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //taking inputs and assinging to variables
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Yıl Giriniz :");
+        int year = scanner.nextInt();
+        //if statement to being or not a leap year and printing output
+        if(year%4==0 && year%100!=0){
+            System.out.println(year+" bir artık yıldır !");
+        } else if (year%400==0) {
+            System.out.println(year+" bir artık yıldır !");
+        }else System.out.println(year+" bir artık yıl değildir !");
     }
 }
