9b7eb90c1c1b3ada8622446e4f6c16ffbafaf579
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-05 06:45:19 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        float kdvRate = 0, kdv = 0;
+
+        //taking inputs and assinging to variables
+        System.out.println("Tutar giriniz: ");
+        float amount = input.nextFloat();
+
+        //kdv conditions with if statement
+        if (amount<1000){
+            kdvRate = (float) 0.18;
+        } else if (amount>=1000) {
+            kdvRate = (float) 0.08;
+        }
+
+        //calculating kdv
+        kdv = kdvRate * amount;
+
+        //printing kdv
+        System.out.println("KDV tutarı: "+kdv);
     }
 }
