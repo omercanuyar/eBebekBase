d52cf1c409820ce8ed577531e374168af591cefa
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-08 00:55:16 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //taking variables and assinging them
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dik a kenarı uzunluğu: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("Dik b kenarı uzunluğu: ");
+        double b = scanner.nextDouble();
+
+        //calculating hypotenuse
+        double hypotenuse = Math.sqrt((a*a)+(b*b));
+
+        //calculating PerimeterOfTriangle 2u / 2 = u
+        double PerimeterOfTriangle = hypotenuse + a + b;
+        double u = PerimeterOfTriangle / 2;
+
+        //calculating area of triangle with -> area * area = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+        double areaOfTriangle = Math.sqrt(u * (u-a) * (u-b) * (u-hypotenuse));
+
+        //printing area of triangle and hypotenuse
+        System.out.println("Hipotenüz uzunluğu: "+hypotenuse);
+        System.out.println("Üçgenin alanı: " + areaOfTriangle);
+
     }
 }
