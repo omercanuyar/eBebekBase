f683c8d33ebde1600481aeda92f359c2ac6f0223
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-08 01:21:57 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        //taking variables and assinging them
+        System.out.println("Gidilen mesafe kaç KM? :");
+        double pathLength = scanner.nextDouble();
+
+        double openingFee = 10;
+        double feePerKM = 2.2;
+        double totalFee;
+
+        //controlling under 20 Liras with if statement
+        if((pathLength * 2.2) + 10 < 20){
+            totalFee = 20;
+        }else totalFee = (pathLength * 2.2) + 10;
+
+        //printing Total Fee
+        System.out.println("Yapılacak ödeme: "+totalFee);
     }
 }
