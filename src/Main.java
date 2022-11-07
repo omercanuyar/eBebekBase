3597b72f75463f0cc412d7b5de2015d04a912a9b
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-08 01:56:43 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //taking variables and assinging them
+        double pi = 3.14;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Yarıçapı giriniz: ");
+        double radius = scanner.nextDouble();
+
+        System.out.println("Merkez açısının ölçüsü giriniz: ");
+        double slice = scanner.nextDouble();
+
+        //calculating area and area of slice
+        double area = radius * radius * pi;
+
+        double sliceArea = area * slice / 360;
+
+        //printing area and area of slice
+        System.out.println("Yarıçapı "+radius+" olan dairenin alanı: "+area);
+        System.out.println("Yarıçapı "+radius+" olan, merkez açısının ölçüsü olan daire diliminin alanı: "+sliceArea);
     }
 }
