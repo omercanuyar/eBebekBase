49b22f4cd9b3a9aca74051cc2f94343080c0ff93
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-19 21:11:43 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int heat;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hava Sıcaklığını giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz");
+
+        }else if ((heat >= 5) && (15 >= heat)) {
+            System.out.println("Sinemaya gidebilirsiniz");
+
+        }else if ((heat >= 15) && (25 >= heat)) {
+            System.out.println("Pikniğe gidebilirsiniz");
+
+        }else if (heat > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz");
+        }
     }
 }
