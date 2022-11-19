e505e49625aaeb5dac8eb9dc2153e5b46521f7bb
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-19 22:03:24 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        int[] array = new int[3];
+
+        System.out.println("3 sayı yazınız: (Her sayıdan sonra Enter'a basınız.)");
+
+        for (int i=0; i<3; i++){
+            array[i] = input.nextInt();
+        }
+
+        Arrays.sort(array);
+
+        for (int j=0; j<3; j++){
+            System.out.println(array[j] + " ");
+        }
+
+
     }
 }
