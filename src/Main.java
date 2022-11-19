f701fbd528cf70c214424b2412af7999aa946894
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-19 21:03:13 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    static int mathGrade, physicGrade, turkishGrade, chemicalGrade,
+            musicGrade, countedLesson = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] lessons = {"math","turkish","physic","chemical","music"};
+
+        for(String lesson : lessons) {
+            setLessonGrade(lesson);
+        }
+
+        int totalGrade = mathGrade + turkishGrade + physicGrade + chemicalGrade + musicGrade;
+        float average = (float) totalGrade / countedLesson;
+
+        System.out.println("Average: " + average);
+
+        if(average >= 55 ) {
+            System.out.println("Congratulations, you passed!");
+        } else {
+            System.out.println("Sorry, you failed!");
+        }
+    }
+
+    public static void setLessonGrade(String lesson){
+        System.out.print("Your " + lesson + " grade(0-100): ");
+        int inputValue = input.nextInt();
+        if(inputValue >= 0 && inputValue <=100) {
+            switch (lesson) {
+                case "math" -> {
+                    mathGrade = inputValue;
+                    countedLesson++;
+                }
+                case "turkish" -> {
+                    turkishGrade = inputValue;
+                    countedLesson++;
+                }
+                case "physic" -> {
+                    physicGrade = inputValue;
+                    countedLesson++;
+                }
+                case "chemical" -> {
+                    chemicalGrade = inputValue;
+                    countedLesson++;
+                }
+                case "music" -> {
+                    musicGrade = inputValue;
+                    countedLesson++;
+                }
+            }
+        } else {
+            System.out.println("Please type a valid grade!");
+            setLessonGrade(lesson);
+        }
     }
 }
