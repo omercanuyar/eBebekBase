287df27e39e768dfcf2887dbf1b027f1d4be8b90
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-19 15:04:29 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String userName, oldPassword, newPassword;
+        int select;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Kullanıcı adınız:");
+        userName = input.nextLine();
+
+        System.out.println("Şifreniz:");
+        oldPassword = input.nextLine();
+
+        if (userName.equals("patika") && oldPassword.equals("java123")){
+            System.out.println("Hoşgeldiniz!");
+        } else if (userName.equals("patika") && !oldPassword.equals("java123")){
+            System.out.println("Yanlış şifre girdiniz!");
+            System.out.println("Yeni şifre oluşturmak ister misiniz?\nEvet için 1'i,\nHayır için 2'yi tuşlayınız.");
+            select = input.nextInt();
+            switch (select){
+                case 1:
+                    System.out.println("Yeni şifrenizi giriniz:");
+                    newPassword = input.nextLine();
+                    if(oldPassword.equals(newPassword)){
+                        System.out.println("Şifreniz eskisiyle aynı olamaz.");
+                        break;
+                    }else{
+                        System.out.println("Şifrenizi başarıyla oluşturdunuz.");
+                        break;
+                    }
+                case 2:
+                    break;
+            }
+        }
     }
 }
