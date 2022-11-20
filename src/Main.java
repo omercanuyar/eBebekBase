e1b3f342f98e0d3e8de1886f77c3e06bb8b0a30d
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-20 03:33:17 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //assinging variables
+        int counter = 0, i, j;
+
+        //finding prime numbers with for loop
+        for(i = 2; i <=100 ; i++){
+
+            for(j = 2; j < i; j++) {
+
+                if(i % j == 0){
+                    counter++;
+                    break;
+                }
+            }
+
+            //printing found prime numbers
+            if(counter == 0)
+
+                System.out.print(i + " ");
+
+            counter = 0;
+        }
     }
 }
