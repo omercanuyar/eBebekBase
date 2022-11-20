4d59e3b46991bb73d5a9837f7c5200af54eddaa6
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-20 04:08:11 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+import java.util.Scanner;
+
+ class Main {
+     static int n1=0,n2=1,n3=0;
+     static void printFibonacci(int count){
+         if(count>0){
+             n3 = n1 + n2;
+             n1 = n2;
+             n2 = n3;
+             System.out.print(" "+n3);
+             printFibonacci(count-1);
+         }
+     }
+     public static void main(String args[]){
+         //Taking variables and assinging them
+         Scanner scan = new Scanner(System.in);
+
+         System.out.println("Kaç elemanlı bir Fibonacci Serisi istediğinizi giriniz: ");
+         int count = scan.nextInt();
+
+         System.out.print(n1+" "+n2);//printing 0 and 1
+         printFibonacci(count-2);//n-2 because 2 numbers are already printed
+     }
+ }
