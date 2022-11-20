92c5fd78484b5da9d8d66abbe512b2939f6f2049
Ömer Can Uyar <48405836+omercanuyar@users.noreply.github.com>
2022-11-20 13:24:47 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //taking inputs from inputs and assinging them
+        int count=0,sum=0;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Sınır belirleyiniz: ");
+        int number = scan.nextInt();
+
+        //sum all numbers that can divide 3 OR 4.
+        for (int i=0;i<=number;i++){
+            if(i%12==0){
+                sum += i;
+                count++; //counter count
+            }
+        }
+        //printing average of sums
+        System.out.println("Ortalamaları: "+(sum/count));
     }
 }
