f5f62bd15491af2dee8f18cfbbe442911a522e0d
Ömer Can Uyar <omercanuyar@ogr.iu.edu.tr>
2024-01-09 15:32:33 +0300

    done
    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please write a number: If you write an odd number, it will stop.");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> ciftSayilar = new ArrayList<>();
+        ArrayList<Integer> ciftVeDordunKatiSayilar = new ArrayList<>();
+        int sum = 0;
+
+        while (true){
+
+            int number = scanner.nextInt();
+
+            if (number % 2 == 0) {
+                ciftSayilar.add(number);
+            } else {
+                break;
+            }
+        }
+
+        for (int ciftSayi : ciftSayilar){
+            if(ciftSayi % 4 == 0)
+            ciftVeDordunKatiSayilar.add(ciftSayi);
+        }
+
+        if(Objects.nonNull(ciftVeDordunKatiSayilar)){
+            for (int ciftVeDordunKatiSayi : ciftVeDordunKatiSayilar) {
+                sum += ciftVeDordunKatiSayi;
+            }
+        }
+
+        System.out.println("The sum of your list: "+ sum);
+
     }
 }
